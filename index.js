be723ae8df0c58a15c4da5f4559563179942c2b8
N.K. <hukuuu@gmail.com>
2014-01-16 23:17:23 +0200

    parameterizing

--- index.js
+++ index.js
@@ -2,24 +2,37 @@ var fs = require('fs'),
   JSONStream = require('JSONStream'),
   es = require('event-stream'),
   through = require('through'),
-  ddlParser = require('./jsontoddl')('accounts')
+  validUrl = require('valid-url'),
+  request = require('request'),
+  argv = require('optimist')
+    .usage('Converts json array to sql insert statements.')
+    .demand(['i', 'o', 't'])
+    .describe('i', 'The .json file for conversion ( may be a url pointing to json service ).')
+    .describe('o', 'The location to put the sql.')
+    .describe('t', 'The table name for the sql.')
+    .argv,
+
+  input = argv.i,
+  output = argv.o,
+  tableName = argv.t,
+  ddlParser = require('./jsontoddl')(tableName)
 
 
-var fileStream = fs.createReadStream(__dirname + '/testFile.json'),
+var inputStream = validUrl.isUri(input) ? request(input) : fs.createReadStream(input),
   parser = JSONStream.parse('*'),
-  logger = es.mapSync(function (data) {
-    console.log('---');
-    console.log(data);
+  logger = es.mapSync(function(data) {
+    console.log('---')
+    console.log(data)
     return data
   })
 
-fileStream
-  .pipe(parser)
-  .pipe(ddlParser)
-  .pipe(logger)
-  .pipe(fs.createWriteStream(__dirname+'/parsed.txt'))
+  inputStream
+    .pipe(parser)
+    .pipe(ddlParser)
+  // .pipe(logger)
+  .pipe(fs.createWriteStream(output))
 
 
-  ////////////
-  // #!node //
-  ////////////
+    ////////////
+    // #!node //
+    ////////////
